3c41a4f47aa96a465dd35e9d811113643df99a92
MU-邵 <315409098@qq.com>
2018-03-15 17:09:47 +0800

    添加vue小程序框架

---
+++ mpvue/src/pages/newpage/index.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="newone">
+    <span>{{tex}}</span>
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  components: {
+    card
+  },
+
+  data () {
+    return {
+      tex: '这是一个新的页面aa'
+    }
+  },
+
+  created () {
+  }
+}
+</script>
+
+<style lang="scss" scoped="" type="text/css" scoped>
+  .newone {
+    width: 100%;
+    height: 200px;
+    text-align: center;
+    line-height: 200px;
+    background: red;
+    span {
+      font-size: 30px;
+      color: white;
+    }
+  }
+</style>
